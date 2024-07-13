27cf2a5b152d6de359676d8cbb6caaf36a22acc9
AdonayLorenzo <145705493+AdonayLorenzo@users.noreply.github.com>
2024-07-13 12:21:19 +0100

    Partida

---
+++ Assets/GameController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] firstPhaseEnemies = new GameObject[3];
+    public GameObject currentEnemy;
+    public int maxEnemyHealth = 15;
+    public int maxEnemyDamage = 2;
+    public int nextEnemyHealthDifference=3;
+    public int nextEnemyDamageDifference=1;
+    public GameObject player;
+    public Transform spawnPoint;
+    public GameObject camera;
+
+    void Start()
+    {
+        InvokeRepeating("startCombat", 2.0f, 2.0f);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+  public void startCombat()
+    {
+        player.GetComponent<Stats>().attack(currentEnemy.GetComponent<Stats>());
+        currentEnemy.GetComponent<Stats>().attack(player.GetComponent<Stats>());
+
+        if (currentEnemy.GetComponent<Stats>().health <= 0)
+        {
+            CancelInvoke("startCombat");
+            Invoke("MoveCharacter", 2.0f);
+            Invoke("SpawnNewEnemy", 4.0f);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        Debug.Log("Moving the character to the right");
+       Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
+       
+        if (rb2d != null)
+        {
+            
+            rb2d.velocity = new Vector2(2.0f, rb2d.velocity.y);
+        }
+        else
+        {
+            
+            player.transform.Translate(Vector3.right * 2.0f * Time.deltaTime);
+
+        }
+        Invoke("DestroyPlayer",2.0f);
+        Invoke("moveCamera",2.0f);
+    }
+
+    void DestroyPlayer(){
+        Debug.Log("Destroying the player");
+        Destroy(player);
+    }
+
+    void SpawnNewEnemy()
+    {
+        int randomIndex = Random.Range(0, firstPhaseEnemies.Length);
+        GameObject newEnemy = Instantiate(firstPhaseEnemies[randomIndex], spawnPoint.position, Quaternion.identity);
+
+        maxEnemyHealth += nextEnemyHealthDifference;
+        maxEnemyDamage += nextEnemyDamageDifference;
+
+        newEnemy.GetComponent<Stats>().health = maxEnemyHealth;
+        newEnemy.GetComponent<Stats>().damage = maxEnemyDamage;
+
+        newEnemy.GetComponent<Stats>().enemyStats = player.GetComponent<Stats>();
+        player.GetComponent<Stats>().enemyStats = newEnemy.GetComponent<Stats>();
+
+        Destroy(currentEnemy);
+        currentEnemy = newEnemy;
+
+        //Invoke("RestartCombat", 2.0f);
+    }
+
+    void RestartCombat()
+    {
+        InvokeRepeating("startCombat", 2.0f, 2.0f);
+    }
+
+   void moveCamera()
+{
+    Transform cameraTransform = camera.GetComponent<Transform>();  // Obtener el componente Transform de la cámara
+    
+    
+    cameraTransform.position = new Vector3(10,0,0);
+}
+
+}
+
