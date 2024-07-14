d5df3de4becb7755b30e55e43ca30271663cd11a
DevPatata <anavaro.sala@gmail.com>
2024-07-14 03:21:32 +0200

    Primer nivel Jugable

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,50 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject[] firstPhaseEnemies = new GameObject[3];
-    public GameObject currentEnemy;
-    public int maxEnemyHealth = 15;
-    public int maxEnemyDamage = 2;
-    public int nextEnemyHealthDifference=3;
-    public int nextEnemyDamageDifference=1;
-    public GameObject player;
-    public Transform spawnPoint;
-    public GameObject gamecamera;
-    public CardManager cardAdder;
-    public GameObject currentPlayer;
-    public GameObject player2;
+    public GameObject[] Enemy;
+    GameObject currentEnemy;
+    int currentEnemyCount = 1;
+    Animator enemyAnim;
 
+    public GameObject player;
+    Animator playerAnim;
     int  PlayerDefence = 0;
 
-    Animator playerAnim;
+    public GameObject gamecamera;
+    Animator cameraAnim;
+
+    public CardManager cardAdder;
+
+
 
     void Start()
     {
-        InvokeRepeating("startCombat", 2.0f, 2.0f);
+        currentEnemy = Enemy[0];
+        Invoke("gameStart", 0.5f);
         playerAnim = player.GetComponent<Animator>();
+        cameraAnim = gamecamera.GetComponent<Animator>();
     }
 
-    
-    void Update()
+    void gameStart()
     {
-        
-    }
-
-    public void secuenciaTrasCombate(){
-        CancelInvoke("startCombat");
-                Destroy(currentEnemy);
-                Debug.Log("Enviando a caminar");
-                Invoke("MovePlayer", 2.0f);
-                Invoke("SpawnNewEnemy", 4.0f);
+        InvokeRepeating("startCombat", 2.0f, 2.0f);
     }
 
     public void startCombat()
     {
         playerAnim.SetBool("IsAttacking", true);
         player.GetComponent<Stats>().attack(currentEnemy.GetComponent<Stats>());
+        Invoke("setAttcBool", 0.3f);
         if(PlayerDefence <= 0)
         {
             currentEnemy.GetComponent<Stats>().attack(player.GetComponent<Stats>());
@@ -58,7 +52,6 @@ public class GameController : MonoBehaviour
             PlayerDefence --;
 
         
-        Invoke("setAttcBool", 0.3f);
     }
 
     public void CardCombat(Card card)
@@ -66,13 +59,13 @@ public class GameController : MonoBehaviour
         switch(card.cardType.ToString()){
             case "Attack":
                 playerAnim.SetBool("IsAttacking", true);
-                player.GetComponent<Stats>().attack(card.value);
+                currentEnemy.GetComponent<Stats>().recieveDamage(card.value);
+                Invoke("setAttcBool", 0.3f);
 
                 if (currentEnemy.GetComponent<Stats>().health <= 0)
                 {
                     secuenciaTrasCombate();
                 }
-                Invoke("setAttcBool", 0.3f);
                 break;
             case "Defense":
                 PlayerDefence ++;
@@ -86,7 +79,7 @@ public class GameController : MonoBehaviour
                 break;
         }
     }
-
+    
     void setAttcBool()
     {
         playerAnim.SetBool("IsAttacking", false);
@@ -97,89 +90,40 @@ public class GameController : MonoBehaviour
         playerAnim.SetBool("isHealing", false);
     }
 
-    void MovePlayer()
-    {
-        Debug.Log("Moviendose");
-        Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
-       
-        if (rb2d != null)
+    void secuenciaTrasCombate(){
+
+        if(currentEnemyCount >= Enemy.Length)
         {
-            Debug.Log(rb2d.velocity);
-            rb2d.velocity = new Vector2(2.0f, rb2d.velocity.y);
-            Debug.Log(rb2d.velocity);
+            Destroy(currentEnemy);
+            playerAnim.SetBool("endScene", true);
+            cameraAnim.SetBool("endScene", true);
+            Invoke("cambioescene", 3.0f);
         }
         else
         {
-            Debug.Log("Moviendo el transform");
-            player.transform.Translate(Vector3.right * 2.0f * Time.deltaTime);
-
+            Destroy(currentEnemy);
+            Invoke("newEnemy", 0.3f);
         }
-        Invoke("DestroyPlayer",2.0f);
-        Invoke("changeCurrentPlayer",3.0f);
-        Invoke("moveCamera",2.0f);
-        StartCoroutine("MoveAndHandleNext");
-
     }
 
-    void DestroyPlayer(){
-        Destroy(currentPlayer);
-    }
-
-    
-    IEnumerator MoveAndHandleNext()
+    void newEnemy()
     {
-       yield return new WaitForSeconds(5.5f);
-       StartCoroutine("MoverPersonaje2");
+        currentEnemy = Enemy[currentEnemyCount];
+        currentEnemy.transform.position = new Vector3(0.5f, - 0.37f, -0.4f);
+        currentEnemyCount++;
     }
 
-    void SpawnNewEnemy()
+    void cambioescene()
     {
-        int randomIndex = Random.Range(0, firstPhaseEnemies.Length);
-        GameObject newEnemy = Instantiate(firstPhaseEnemies[randomIndex], spawnPoint.position, Quaternion.identity);
+        Scene scene = SceneManager.GetActiveScene();
 
-        maxEnemyHealth += nextEnemyHealthDifference;
-        maxEnemyDamage += nextEnemyDamageDifference;
-
-        newEnemy.GetComponent<Stats>().health = maxEnemyHealth;
-        newEnemy.GetComponent<Stats>().damage = maxEnemyDamage;
-
-        newEnemy.GetComponent<Stats>().enemyStats = player.GetComponent<Stats>();
-        player.GetComponent<Stats>().enemyStats = newEnemy.GetComponent<Stats>();
-
-        Destroy(currentEnemy);
-        currentEnemy = newEnemy;
-
-        //Invoke("RestartCombat", 2.0f);
-    }
-
-    void RestartCombat()
-    {
-        InvokeRepeating("startCombat", 2.0f, 2.0f);
-    }
-
-   void moveCamera()
-{
-    Transform cameraTransform = gamecamera.GetComponent<Transform>();  
-    cameraTransform.position = new Vector3(10,0,-10);
-}
-
-    IEnumerator MoverPersonaje2(){
-     
-     float moveDuration = 1.5f;
-     float elapsedTime = 0f;
-
-        while (elapsedTime < moveDuration)
+        if (scene.name == "Card_Scene")
         {
-            player2.transform.Translate(Vector3.right * 1.5f * Time.deltaTime);
-            elapsedTime += Time.deltaTime;
-            yield return null; 
+            SceneManager.LoadScene("Second_Scene");
         }
-    }
-    void changeCurrentPlayer(){
-        if(currentPlayer == player){
-            currentPlayer = player2;
-        }else{
-            //currentPlayer = player3;
+        else if (scene.name == "Second_Scene")
+        {
+            SceneManager.LoadScene("SalaDelTrono");
         }
     }
 
